2b49a0eca90e8a8569b29f123e0b6e7bf45a4101
JuniorWilder <juniorwilder4@gmail.com>
2023-09-14 21:55:33 -0300

    iniciando rota get

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const http = require('http')  //https://nodejs.org/dist/latest-v18.x/docs/api/synopsis.html
+//const http = require('http')  //https://nodejs.org/dist/latest-v18.x/docs/api/synopsis.html
+import { get, createServer } from "http";
 
 const HOST = 'localhost'
 const PORT = 5000
 
-const server = http.createServer((req, res) => {
+const server = createServer((req, res) => {
     res.end('Oi, estou funcionando com nodemon!')
+})
+
+get('/teste', (req, res) => {
+    return 'Teste'
 })
 
 server.listen(PORT, HOST, () => {
