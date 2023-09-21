a565ed2b83ff2b520dac41b1477bcc23b6175bae
JuniorWilder <juniorwilder4@gmail.com>
2023-09-21 18:44:58 -0300

    http modulo

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 //const http = require('http')  //https://nodejs.org/dist/latest-v18.x/docs/api/synopsis.html
-import { get, createServer } from "http";
+
+import http from "http";
 
 const HOST = 'localhost'
 const PORT = 5000
 
-const server = createServer((req, res) => {
-    res.end('Oi, estou funcionando com nodemon!')
+const server = http.createServer((req, res) => {
+    
+    console.log(req.method);
+
+    if(req.method === 'GET') {
+        if(req.url === '/usuarios'){
+            res.writeHead(200, {'Content-type': 'text/plain'})
+            res.end('Entrou no /usuarios')
+        } else {
+            res.end('Oi, estou funcionando com nodemon!')
+        }
+    } 
+
+    
 })
 
-get('/teste', (req, res) => {
-    return 'Teste'
-})
+
 
 server.listen(PORT, HOST, () => {
     console.log(`Servidor Rodando: http://${HOST}:${PORT}/`);
